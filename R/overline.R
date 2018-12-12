d4093186767230a5b1e139bda02afc110ab197cd
Malcolm Morgan <mem48@users.noreply.github.com>
2018-12-12 17:11:37 +0000

    Clean Code

--- R/overline.R
+++ R/overline.R
@@ -1,64 +1,7 @@
-# Convert linestrings to many straight lines
-line2segments = function(x, ncores = 1){
-  c1 = st_coordinates(x) # Convert SF to matrix
-  l1 = c1[,3] # Get which line each point is part of
-  l1_start = duplicated(l1) # find the break points between lines
-  l1_start = c(l1_start[2:length(l1)],FALSE)
-  seqs = seq(1,length(l1),1)[l1_start] # Make list of linestrings
-  if(ncores == 1){
-    geoms = pbapply::pblapply(seqs, function(y){sf::st_linestring(c1[c(y,y+1),c(1,2)])})
-  }else{
-    cl = parallel::makeCluster(ncores)
-    parallel::clusterExport(cl=cl, varlist=c("c1"), envir = environment())
-    #parallel::clusterEvalQ(cl, {library(sf)})
-    #geoms = pbapply::pblapply(seqs, function(y){sf::st_linestring(c1[c(y,y+1),c(1,2)])}, cl = cl)
-    geoms = parallel::parLapply(cl = cl, seqs, function(y){sf::st_linestring(c1[c(y,y+1),c(1,2)])})
-    parallel::stopCluster(cl)
-  }
-  geoms = st_as_sfc(geoms, crs = st_crs(x))
-  dat = x # extract attributes
-  st_geometry(dat) = NULL
-  dat = as.data.frame(dat)
-  dat = dat[l1[l1_start],, drop = FALSE] # repeate attriibutes
-  st_geometry(dat) = geoms # put together
-  return(dat)
-}
-
-# Overlay duplicated lines
-#' @param x SF data frame of linestrings
-#' @param attrib column name to be summed
-#' @param drop_0 remove geometry of lines where summed values is Zero
+#' # Overlay duplicated lines
+#' #' @param x SF data frame of linestrings
+#' #' @param attrib column name to be summed
 #' 
-overline_malcolm = function(x, attrib, ncores = 1){
-  if(all(st_geometry_type(x) != "LINESTRING")){
-    message("Only LINESTRING is supported")
-    stop()
-  }
-  if("matchingID" %in% names(attrib)){
-    message("matchingID is not a permitted column name, please rename that column")
-    stop()
-  }
-  x = x[,attrib]
-  x_split = line2segments(x, ncores = ncores) # Split into segments
-  x_dupe = duplicated(x_split$geometry)
-  times = unique(x_split[,"geometry", drop = FALSE]) # make lookup table
-  times$id = seq(1,nrow(times))
-  x_split$matchingID = times$id[match( x_split$geometry, times$geometry)]
-  x_group = x_split[,c("matchingID",attrib)]
-  st_geometry(x_group) = NULL 
-  x_group = x_group %>%
-    group_by(matchingID) %>%
-    summarise_all(funs(sum), na.rm = TRUE)
-  x_nodupe = x_split[!x_dupe,]
-  #x_nodupe$value = NULL
-  #x_nodupe[[attrib]] = NULL
-  x_nodupe = x_nodupe[,"matchingID"]
-  x_nodupe = left_join(x_nodupe,x_group,"matchingID" )
-  x_nodupe = x_nodupe[,attrib]
-  return(x_nodupe)
-}
-
-
 overline_malcolm2 = function(x, attrib){
   if(all(sf::st_geometry_type(x) != "LINESTRING")){
     message("Only LINESTRING is supported")
@@ -87,14 +30,6 @@ overline_malcolm2 = function(x, attrib){
   message(paste0(Sys.time()," removing duplicates"))
   c3_dup = duplicated(c3) # de-duplicate
   c3_nodup = c3[!c3_dup,]
-  # message(paste0(Sys.time()," making df"))
-  # c3_t = data.frame(t(c3))
-  # c3_nodup_t = data.frame(t(c3_nodup))
-  # message(paste0(Sys.time()," matching"))
-  # message(Sys.time())
-  # matchID = match(c3_t, c3_nodup_t) # make looup 
-  # message(Sys.time())
-  #####
   c3_df = as.data.frame(c3)
   names(c3_df) = c("X1","Y1","X2","Y2")
   c3_nodup_df = as.data.frame(c3_nodup)
@@ -103,13 +38,7 @@ overline_malcolm2 = function(x, attrib){
   matchID = dplyr::left_join(c3_df, c3_nodup_df, by = c("X1" = "X1","Y1" = "Y1","X2" = "X2","Y2" = "Y2"))
   matchID = matchID$matchID
   rm(c3_df,c3_nodup_df,c3,c3_dup)
-  #message(Sys.time())
-  
-  ####
-  
-  #matchID2 = row.match(c3, c3_nodup)
-  
-  
+
   # Calcualte the attributes
   message(paste0(Sys.time()," restructuring attributes"))
   x_split = x # extract attributes
@@ -131,7 +60,7 @@ overline_malcolm2 = function(x, attrib){
   st_geometry(x_split) = geoms # put together
   rm(geoms)
   
-  # Recombine in to fewer lines
+  # Recombine into fewer lines
   message(paste0(Sys.time()," simplifying geometry"))
   overlined_simple = x_split %>%
     group_by_at(attrib) %>%
@@ -146,4 +75,68 @@ overline_malcolm2 = function(x, attrib){
   overlined_fin = rbind(overlined_simple_l,overlined_simple_ml)
   return(overlined_fin)
 }
+
+
+#' # Convert linestrings to many straight lines
+#' line2segments = function(x, ncores = 1){
+#'   c1 = st_coordinates(x) # Convert SF to matrix
+#'   l1 = c1[,3] # Get which line each point is part of
+#'   l1_start = duplicated(l1) # find the break points between lines
+#'   l1_start = c(l1_start[2:length(l1)],FALSE)
+#'   seqs = seq(1,length(l1),1)[l1_start] # Make list of linestrings
+#'   if(ncores == 1){
+#'     geoms = pbapply::pblapply(seqs, function(y){sf::st_linestring(c1[c(y,y+1),c(1,2)])})
+#'   }else{
+#'     cl = parallel::makeCluster(ncores)
+#'     parallel::clusterExport(cl=cl, varlist=c("c1"), envir = environment())
+#'     #parallel::clusterEvalQ(cl, {library(sf)})
+#'     #geoms = pbapply::pblapply(seqs, function(y){sf::st_linestring(c1[c(y,y+1),c(1,2)])}, cl = cl)
+#'     geoms = parallel::parLapply(cl = cl, seqs, function(y){sf::st_linestring(c1[c(y,y+1),c(1,2)])})
+#'     parallel::stopCluster(cl)
+#'   }
+#'   geoms = st_as_sfc(geoms, crs = st_crs(x))
+#'   dat = x # extract attributes
+#'   st_geometry(dat) = NULL
+#'   dat = as.data.frame(dat)
+#'   dat = dat[l1[l1_start],, drop = FALSE] # repeate attriibutes
+#'   st_geometry(dat) = geoms # put together
+#'   return(dat)
+#' }
+#' 
+#' # Overlay duplicated lines
+#' #' @param x SF data frame of linestrings
+#' #' @param attrib column name to be summed
+#' #' @param drop_0 remove geometry of lines where summed values is Zero
+#' #' 
+#' overline_malcolm = function(x, attrib, ncores = 1){
+#'   if(all(st_geometry_type(x) != "LINESTRING")){
+#'     message("Only LINESTRING is supported")
+#'     stop()
+#'   }
+#'   if("matchingID" %in% names(attrib)){
+#'     message("matchingID is not a permitted column name, please rename that column")
+#'     stop()
+#'   }
+#'   x = x[,attrib]
+#'   x_split = line2segments(x, ncores = ncores) # Split into segments
+#'   x_dupe = duplicated(x_split$geometry)
+#'   times = unique(x_split[,"geometry", drop = FALSE]) # make lookup table
+#'   times$id = seq(1,nrow(times))
+#'   x_split$matchingID = times$id[match( x_split$geometry, times$geometry)]
+#'   x_group = x_split[,c("matchingID",attrib)]
+#'   st_geometry(x_group) = NULL 
+#'   x_group = x_group %>%
+#'     group_by(matchingID) %>%
+#'     summarise_all(funs(sum), na.rm = TRUE)
+#'   x_nodupe = x_split[!x_dupe,]
+#'   #x_nodupe$value = NULL
+#'   #x_nodupe[[attrib]] = NULL
+#'   x_nodupe = x_nodupe[,"matchingID"]
+#'   x_nodupe = left_join(x_nodupe,x_group,"matchingID" )
+#'   x_nodupe = x_nodupe[,attrib]
+#'   return(x_nodupe)
+#' }
+
+
+
 
