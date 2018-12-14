363f45d1fe8a2a08629ea508249a0194879d5fe1
Malcolm Morgan <mem48@users.noreply.github.com>
2018-12-14 14:49:26 +0000

    new  layers

--- R/import_data.R
+++ R/import_data.R
@@ -20,7 +20,7 @@ if(file.exists("securedata/rf_sf.Rds")){
   saveRDS(rf_shape,"securedata/rf_sf.Rds")
 }
 
-rf_test = rf_shape[1:10000,]
+#rf_test = rf_shape[1:10000,]
 
 message(paste0(Sys.time()," Overlineing"))
 # overlined1 = overline_malcolm(x = rf_test,
@@ -34,19 +34,75 @@ message(paste0(Sys.time()," Buffering"))
 overlined_buf = st_buffer(overlined, 10)
 message(paste0(Sys.time()," Rastering"))
 rasterOptions(maxmemory = 1e+11)
-rast = raster::raster(overlined, resolution = 10)
-raster::dataType(rast) <- "INT2U"
+
 types = c("bicycle","govtarget_slc","dutch_slc")
+grid = st_make_grid(overlined_buf, n = c(2,2))
+rast_100
+#inter = st_intersects(overlined_buf,grid)
+
 for(i in types){
-  message(paste0("Doing ",i))
-  rast_tmp = fasterize(overlined_buf, raster = rast, field = i)
-  raster::writeRaster(rast_tmp,
-                      filename = paste0(i,".tif"),
-                      format = "GTiff", 
+  message(paste0(Sys.time()," Doing ",i))
+  overlined_buf_tmp = overlined_buf[overlined_buf[[i]] > 0, ]
+  rast = raster::raster(overlined_buf, resolution = 10)
+  raster::dataType(rast) <- "INT2U"
+  rast = fasterize(overlined_buf_tmp, raster = rast, field = i)
+  # Save Tiles for main raster
+  for(j in 1:length(grid)){
+    extent = st_bbox(grid[j])
+    extent = c(extent[1],extent[3],extent[2],extent[4])
+    rast_crop = raster::crop(rast,extent)
+    message(paste0(Sys.time()," saving Raster ",i,"-10m-",j))
+    raster::writeRaster(rast_crop,
+                        filename = paste0("rasters/",i,"-10m-",j,".tif"),
+                        format = "GTiff",
+                        datatype = "INT2U",
+                        overwrite = T)
+    rm(rast_crop, extent)
+  }
+  rast_50 = raster::aggregate(rast_tmp, fact=5, fun=max, na.rm=T)
+  message(paste0(Sys.time()," saving Raster ",i,"-50m"))
+  raster::writeRaster(rast_50,
+                      filename = paste0("rasters/",i,"-50m.tif"),
+                      format = "GTiff",
+                      datatype = "INT2U",
                       overwrite = T)
-  rm(rast_tmp)
+  rm(rast_50)
+  rast_100 = raster::aggregate(rast_tmp, fact=10, fun=max, na.rm=T)
+  message(paste0(Sys.time()," saving Raster ",i,"-100m"))
+  raster::writeRaster(rast_50,
+                      filename = paste0("rasters/",i,"-100m.tif"),
+                      format = "GTiff",
+                      datatype = "INT2U",
+                      overwrite = T)
+  rm(rast_100)
+  
 }
 message(paste0(Sys.time()," Done"))
+
+
+# for(i in types){
+#   message(paste0(Sys.time()," Doing ",i))
+#   
+#   for(j in 1:4){
+#     message(paste0(Sys.time()," Doing grid ",j))
+#     overlined_buf_tmp = overlined_buf[sapply(inter, function(y){j %in% y}), ]
+#     overlined_buf_tmp = overlined_buf_tmp[overlined_buf_tmp[[i]] > 0, ]
+#     rast = raster::raster(st_sf(data.frame(id = 1, geometry = grid[j])), resolution = 10)
+#     raster::dataType(rast) <- "INT2U"
+#     rast_tmp = fasterize(overlined_buf_tmp, raster = rast, field = i)
+#     message(paste0(Sys.time()," saving Raster ",i,"-",j))
+#     raster::writeRaster(rast_tmp,
+#                         filename = paste0("rasters/",i,"-",j,"tif"),
+#                         format = "ascii", 
+#                         datatype = "INT2U",
+#                         overwrite = T)
+#     rm(rast_tmp)
+#   }
+#   
+#   
+#   
+# }
+# message(paste0(Sys.time()," Done"))
 
 # point = st_centroid(overlined_fin[1,])
 # point = st_buffer(point, 100000)
