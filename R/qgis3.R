d7f7d9d718a36e1eda4108d4436ce612558a3cae
Malcolm Morgan <mem48@users.noreply.github.com>
2019-02-15 09:41:03 +0000

    Bug fix

--- R/qgis3.R
+++ R/qgis3.R
@@ -41,18 +41,18 @@ for(i in 1:3){
 
 for(i in 1:3){
   message(paste0(Sys.time()," tiling ",scenarios[i]))
-  message(paste0(Sys.time()," tiling 100m rater"))
-  params_tiles$INPUT = paste0(getwd(),"/rasters/,",scenarios[i],"-100m.tif")
+  message(paste0(Sys.time()," tiling 100m raster"))
+  params_tiles$INPUT = paste0(getwd(),"/rasters/",scenarios[i],"-100m.tif")
   params_tiles$ZOOM = "1-9"
   params_tiles$OUTPUTDIR = paste0(getwd(),"/tiles/",scenarios[i])
   run_qgis(alg_tiles, params = params_tiles, load_output = FALSE)
-  message(paste0(Sys.time()," tiling 50m rater"))
-  params_tiles$INPUT = paste0(getwd(),"/rasters/,",scenarios[i],"-50m.tif")
+  message(paste0(Sys.time()," tiling 50m raster"))
+  params_tiles$INPUT = paste0(getwd(),"/rasters/",scenarios[i],"-50m.tif")
   params_tiles$ZOOM = "10-11"
   run_qgis(alg_tiles, params = params_tiles, load_output = FALSE)
-  message(paste0(Sys.time()," tiling 10m rater"))
-  params_tiles$INPUT = paste0(getwd(),"/rasters/,",scenarios[i],"-10m-merge.tif")
-  params_tiles$ZOOM = "12-18"
+  message(paste0(Sys.time()," tiling 10m raster"))
+  params_tiles$INPUT = paste0(getwd(),"/rasters/",scenarios[i],"-10m-merge.tif")
+  params_tiles$ZOOM = "12-15"
   run_qgis(alg_tiles, params = params_tiles, load_output = FALSE)
 }
 
